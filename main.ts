8310ded7535e6c34b5c114e3e46e805f4e29506d
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-11-05 13:02:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
-basic.clearScreen()
-basic.forever(function () {
+input.onPinPressed(TouchPin.P1, function () {
     robotbit.Servo(robotbit.Servos.S1, 180)
     basic.pause(1000)
     robotbit.Servo(robotbit.Servos.S1, 90)
+    basic.pause(1000)
+})
+let _switch = 0
+basic.clearScreen()
+basic.forever(function () {
+    _switch = pins.digitalReadPin(DigitalPin.P1)
 })
