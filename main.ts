46a101ef503ca4ad8b1b182f556ce24abe9c29e0
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-11-05 14:31:55 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
-let dial = 0
+let distance_from_door = 0
 basic.clearScreen()
 basic.forever(function () {
-    dial = pins.digitalReadPin(DigitalPin.P1)
-    robotbit.Servo(robotbit.Servos.S1, dial)
-    basic.pause(1000)
-    robotbit.Servo(robotbit.Servos.S1, dial)
+    distance_from_door = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    if (distance_from_door < 5) {
+        robotbit.Servo(robotbit.Servos.S1, 90)
+        basic.pause(1000)
+    }
+    if (distance_from_door > 5) {
+        robotbit.Servo(robotbit.Servos.S1, 10)
+    }
     basic.pause(1000)
 })
